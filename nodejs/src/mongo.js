f2bad52fce9765c6463cefdfb23222b765497078
JBanks00 <46931166+JBanks00@users.noreply.github.com>
2022-03-21 10:41:29 +0000

    Added code to mongojs for subscriber nodes

--- nodejs/src/mongo.js
+++ nodejs/src/mongo.js
@@ -1,4 +1,44 @@
 //Object data modelling library for mongo
+
+
+
+//Connect to MongoJS - Subscriber nodes
+amqp.connect('mongodb://localmongo1:27017,localmongo2:27017,localmongo3:27017/NotFLIXDB?replicaSet=rs0', function(error0, connection) {
+      if (error0) {
+              throw error0;
+            }
+      connection.createChannel(function(error1, channel) {
+              if (error1) {
+                        throw error1;
+                      }
+              var exchange = 'logs';
+
+              channel.assertExchange(exchange, 'fanout', {
+                        durable: false
+                      });
+
+              channel.assertQueue('', {
+                        exclusive: true
+                      }, function(error2, q) {
+                                if (error2) {
+                                            throw error2;
+                                          }
+                                console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+                                channel.bindQueue(q.queue, exchange, '');
+
+                                channel.consume(q.queue, function(msg) {
+                                            if(msg.content) {
+                                                            console.log(" [x] %s", msg.content.toString());
+                                                          }
+                                          }, {
+                                                      noAck: true
+                                                    });
+                              });
+            });
+});
+
+//Publisher node interval of 5 seconds
+
 const mongoose = require('mongoose');
 
 //Mongo db client library
