c5ce59c7aff406b616882b69903a0e1a0cee6672
JBanks00 <46931166+JBanks00@users.noreply.github.com>
2022-03-21 11:43:07 +0000

    Added subscriber code

--- nodejs/src/mongo.js
+++ nodejs/src/mongo.js
@@ -1,7 +1,34 @@
 //Object data modelling library for mongo
 
 //Subscirber nodes MongoDB 
-    
+
+db.open(function(err) {
+    if (err) throw err;
+
+    db.collection('messages', function(err, collection) {
+        if (err) throw err;
+
+        var latest = collection.find({}).sort({ $natural: -1 }).limit(1);
+
+        latest.nextObject(function(err, doc) {
+            if (err) throw err;
+
+            var query = { _id: { $gt: doc._id }};
+            
+            var options = { tailable: true, awaitdata: true, numberOfRetries: -1 };
+            var cursor = collection.find(query, options).sort({ $natural: 1 });
+
+            (function next() {
+                cursor.nextObject(function(err, message) {
+                    if (err) throw err;
+                    console.log(message);
+                    next();
+                });
+            })();
+        });
+    });
+});
+
 //Publisher node interval of 5 seconds
 
 const mongoose = require('mongoose');
