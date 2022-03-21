b9e8b5d40681d580a91fb30aee546537d2414fc1
JBanks00 <46931166+JBanks00@users.noreply.github.com>
2022-03-21 11:37:15 +0000

    Revert update

--- nodejs/src/mongo.js
+++ nodejs/src/mongo.js
@@ -24,34 +24,7 @@ const connectionString = 'mongodb://localmongo1:27017,localmongo2:27017,localmon
 
 setInterval(function() {
 
-  connection.createChannel(function(error1, channel) {
-    if (error1) {
-              throw error1;
-            }
-    var exchange = 'logs';
-
-    channel.assertExchange(exchange, 'fanout', {
-              durable: false
-            });
-
-    channel.assertQueue('', {
-              exclusive: true
-            }, function(error2, q) {
-                      if (error2) {
-                                  throw error2;
-                                }
-                      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
-                      channel.bindQueue(q.queue, exchange, '');
-
-                      channel.consume(q.queue, function(msg) {
-                                  if(msg.content) {
-                                                  console.log(" [x] %s", msg.content.toString());
-                                                }
-                                }, {
-                                            noAck: true
-                                          });
-                    });
-  });
+  console.log(`Intervals are used to fire a function for the lifetime of an application.`);
 
 }, 3000);
 
