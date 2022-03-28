47795b23698ba603a1ebba81186e38ebbe907fce
JBanks00 <46931166+JBanks00@users.noreply.github.com>
2022-03-28 10:43:18 +0100

    Added check if leader and create subs

--- nodejs/src/mongo.js
+++ nodejs/src/mongo.js
@@ -1,23 +1,73 @@
 //Object data modelling library for mongo
 
 //Subscirber Nodes MongoDB 
+systemLeader = 0
 
-//Hostname of the Node
+//Get the hostname of the node
 var os = require("os");
 var myhostname = os.hostname();
 
-//Message To Send
 var nodeID = Math.floor(Math.random() * (100 - 1 + 1) + 1);
 toSend = {"hostname" : myhostname, "status": "alive","nodeID":nodeID} ;
 
-//Publish a Status Message 
+//based on the interval publish a status message 
 setInterval(function() {
   console.log("sending alive");
   sock.send(["status", JSON.stringify(toSend)]);
 }, 500);
 
 
-//Publisher Node
+//check if leader
+setInterval(function() {
+  console.log(JSON.stringify(nodes));
+  leader = 1;
+  activeNodes = 0;
+  Object.entries(nodes).forEach(([hostname,prop]) => {
+    console.log("test" + JSON.stringify(hostname) + JSON.stringify(prop) )
+    maxNodeID = nodeID;
+    if(hostname != myhostname){
+      if("nodeID" in prop){
+        activeNodes++;
+        if(prop.nodeID > nodeID)
+        {
+          leader = 0;
+        }
+      }
+    }
+    if((leader == 1) && (activeNodes == (nodes.length - 1)))
+    {
+      systemLeader = 1;
+    }
+  });
+}, 2000);
+
+//for each key value in nodes
+Object.entries(nodes).forEach(([hostname,prop]) => {
+  
+    //print the hostname IP
+    console.log("hostname = " + hostname + " ip = " + prop.ip);
+//create a number of subscribers to connect to publishers
+    var subsockets = [];
+    if(myhostname != hostname ){
+        tempsoc = zmq.socket("sub");
+        tempsoc.connect("tcp://" + prop.ip + ":3000");
+        tempsoc.subscribe("status");
+        console.log("Subscriber connected to port 3000 of " + hostname);
+        tempsoc.on("message", function(topic, message) {
+          jsonMessage = JSON.parse(message.toString("utf-8"));
+            console.log(
+            "received a message from " + hostname + " related to:",
+            topic.toString("utf-8"),
+            "containing message:",
+            jsonMessage.status + " with ID" + jsonMessage.nodeID
+            );
+            nodes[hostname].nodeID =  jsonMessage.nodeID;
+        });
+        //push this instance of a sub socket to the list.
+        subsockets.push(tempsoc);
+    }
+});
+
 
 db.open(function(err) {
     if (err) throw err;
