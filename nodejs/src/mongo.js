71d1660d8d2fe00d16ab49b85eac19a28dba4d56
JBanks00 <46931166+JBanks00@users.noreply.github.com>
2022-04-03 20:17:49 +0100

    Edited Algorithm

--- nodejs/src/mongo.js
+++ nodejs/src/mongo.js
@@ -1,138 +1,32 @@
-//Subscirber Nodes MongoDB 
-systemLeader = 0
+//System Leader Boolean Value
+var systemLeader = false;
+
+//Add Required Libraries
+var amqp = require('amqplib/callback_api');
+const mongoose = require('mongoose');
+const express = require('express')
 
 //Get Hostname
-var amqp = require('amqplib/callback_api');
-var os = require("os");
+const os = require("os");
 var myhostname = os.hostname();
 
-//Generate Random Number and Message
+//Generate Random Number
 var nodeID = Math.floor(Math.random() * (100 - 1 + 1) + 1);
-toSend = {"hostname" : myhostname, "status": "alive","nodeID":nodeID} ;
 
-//Display Status Message
-setInterval(function() {
-  console.log("sending alive");
-  sock.send(["status", JSON.stringify(toSend)]);
-}, 500);
+//Connection String to Connect to Mongo Servers
+const connectionString = 'mongodb://localmongo1:27017,localmongo2:27017,localmongo3:27017/NotFLIXDB?replicaSet=rs0';
 
-//Check for Leader
-setInterval(function() {
-  console.log(JSON.stringify(nodes));
-  leader = 1;
-  activeNodes = 0;
-  Object.entries(nodes).forEach(([hostname,prop]) => {
-    console.log("test" + JSON.stringify(hostname) + JSON.stringify(prop) )
-    maxNodeID = nodeID;
-    if(hostname != myhostname){
-      if("nodeID" in prop){
-        activeNodes++;
-        if(prop.nodeID > nodeID)
-        {
-          leader = 0;
-        }
-      }
-    }
-    if((leader == 1) && (activeNodes == (nodes.length - 1)))
-    {
-      systemLeader = 1;
-    }
-  });
-}, 2000);
-
-//for each key value in nodes
-Object.entries(nodes).forEach(([hostname,prop]) => {
-  
-    //print the hostname IP
-    console.log("hostname = " + hostname + " ip = " + prop.ip);
-//create a number of subscribers to connect to publishers
-    var subsockets = [];
-    if(myhostname != hostname ){
-        tempsoc = zmq.socket("sub");
-        tempsoc.connect("tcp://" + prop.ip + ":3000");
-        tempsoc.subscribe("status");
-        console.log("Subscriber connected to port 3000 of " + hostname);
-        tempsoc.on("message", function(topic, message) {
-          jsonMessage = JSON.parse(message.toString("utf-8"));
-            console.log(
-            "received a message from " + hostname + " related to:",
-            topic.toString("utf-8"),
-            "containing message:",
-            jsonMessage.status + " with ID" + jsonMessage.nodeID
-            );
-            nodes[hostname].nodeID =  jsonMessage.nodeID;
-        });
-        //push this instance of a sub socket to the list.
-        subsockets.push(tempsoc);
-    }
-});
-
-
-db.open(function(err) {
-    if (err) throw err;
-
-    db.collection('messages', function(err, collection) {
-        if (err) throw err;
-
-        var latest = collection.find({}).sort({ $natural: -1 }).limit(1);
-
-        latest.nextObject(function(err, doc) {
-            if (err) throw err;
-
-            var query = { _id: { $gt: doc._id }};
-            
-            var options = { tailable: true, awaitdata: true, numberOfRetries: -1 };
-            var cursor = collection.find(query, options).sort({ $natural: 1 });
-
-            (function next() {
-                cursor.nextObject(function(err, message) {
-                    if (err) throw err;
-                    console.log(message);
-                    next();
-                });
-            })();
-        });
-    });
-});
-
-//Publisher node interval of 5 seconds
-
-const mongoose = require('mongoose');
-
-//Mongo db client library
-//const MongoClient  = require('mongodb');
-
-//Express web service library
-const express = require('express')
+//Connect to Cluster
+app.use(bodyParser.json());
+mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+var Schema = mongoose.Schema;
 
 //used to parse the server response from json to object.
 const bodyParser = require('body-parser');
 
-//instance of express and port to use for inbound connections.
-const app = express()
-const port = 3000
-
-//connection string listing the mongo servers. This is an alternative to using a load balancer. THIS SHOULD BE DISCUSSED IN YOUR ASSIGNMENT.
-const connectionString = 'mongodb://localmongo1:27017,localmongo2:27017,localmongo3:27017/NotFLIXDB?replicaSet=rs0';
-
-setInterval(function() {
-
-  console.log(`Intervals are used to fire a function for the lifetime of an application.`);
-
-}, 3000);
-
-//tell express to use the body parser. Note - This function was built into express but then moved to a seperate package.
-app.use(bodyParser.json());
-
-//connect to the cluster
-mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true});
-
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-var Schema = mongoose.Schema;
-
+//Creation of Schema for NotFLIX
 var NotFLIXSchema = new Schema({
   AccountID: Number,
   Username: String,
@@ -145,32 +39,59 @@ var NotFLIXSchema = new Schema({
 
 var NotFLIXModel = mongoose.model('Items', NotFLIXSchema, 'items');
 
+//Each Node Sends an Alive Message 
+asetInterval(function () {
+  amqp.connect('amqp://test:test@192.168.56.10', function (error0, connection) {
+    if (error0) {
+      throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+      var exchange = 'logs';
+      var msg =  'Node ALive With ID:' + nodeID;
+      channel.assertExchange(exchange, 'fanout', {
+        durable: false
+      });
+      channel.publish(exchange, '', Buffer.from(msg));
+      console.log(" [x] Sent %s", msg);
+    });
+    
+      setTimeout(function() {
+        connection.close();
+      }, 500);
+    });
 
-
-app.get('/', (req, res) => {
-  NotFLIXModel.find({},'item', (err, stock) => {
-    if(err) return handleError(err);
-    res.send(JSON.stringify(stock))
-  }) 
-})
-
-app.post('/',  (req, res) => {
-  var awesome_instance = new SomeModel(req.body);
-  awesome_instance.save(function (err) {
-  if (err) res.send('Error');
-    res.send(JSON.stringify(req.body))
+    //Nodes Subsrcibe to Message Queue
+    amqp.connect('amqp://test:test@192.168.56.10', function(error0, connection) {
+      if (error0) {
+        throw error0;
+      }
+      connection.createChannel(function(error1, channel) {
+        if (error1) {
+          throw error1;
+        }
+        var exchange = 'logs';
+        channel.assertExchange(exchange, 'fanout', {
+          durable: false
+        });
+        channel.assertQueue('', {
+          exclusive: true
+        }, function(error2, q) {
+          if (error2) {
+            throw error2;
+          }
+          console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+          channel.bindQueue(q.queue, exchange, '');
+          channel.consume(q.queue, function(msg) {
+            if(msg.content) {
+              console.log(" [x] %s", msg.content.toString());
+            }
+            }, {
+            noAck: true
+          });
+        });
+      });
+    });
   });
-})
-
-app.put('/',  (req, res) => {
-  res.send('Got a PUT request at /')
-})
-
-app.delete('/',  (req, res) => {
-  res.send('Got a DELETE request at /')
-})
-
-//bind the express web service to the port specified
-app.listen(port, () => {
- console.log(`Express Application listening at port ` + port)
-})
